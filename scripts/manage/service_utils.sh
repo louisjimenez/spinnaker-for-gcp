4a02d4069df3f7c472fca2eb1c4fb16f37746a88
Louis Jimenez <louisjimenez@google.com>
2019-10-03 12:56:31 -0400

    feat(ci): Support for install via CI

--- scripts/manage/service_utils.sh
+++ scripts/manage/service_utils.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
 bold() {
-  echo ". $(tput bold)" "$*" "$(tput sgr0)";
+  if [ "$CI" != true ]; then
+    echo ". $(tput bold)" "$*" "$(tput sgr0)";
+  else
+    echo "$*"
+  fi
 }
 
 has_service_enabled() {
   gcloud services list --project $1 \
     --filter="config.name:$2" \
     --format="value(config.name)"
+}
+
+check_for_command() {
+  COMMAND_PRESENT=$(command -v $1)
+  echo $COMMAND_PRESENT
 }
